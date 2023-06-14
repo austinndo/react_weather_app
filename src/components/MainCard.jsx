a6a564879ea32a2d64fa9515ccf790a6f9ae5267
Austin Do <austinndo@gmail.com>
2023-06-14 16:09:22 -0700

    May change what main card displays. Forecast keeps avg temps, low, high, and icon. Main should display more details

--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react"
 import geodata from "../geodata.json"
 import ForecastCards from "./ForecastCards"
 
 const MainCard = (props) => {
+
+  // let mainWeather = props.currentWeather
+  // let setWeather = props.setCurrentWeather
+
+  // useEffect(() => {
+  //   if (props.mainCard === 0) {
+
+  //   }
+  //   setWeather(geodata.current)
+  // }, [props.mainCard])
+
 
   // #change these to props... when ready to make api calls
   let temp = geodata.current.temp_f
@@ -25,7 +37,14 @@ const MainCard = (props) => {
       </div>
     </div>
   )
-  // (props.mainCard === 0) ? ForecastCards.cardZero : (props.mainCard === 1) ? ForecastCards.cardOne : ForecastCards.cardTwo
+
+  // const mainCardLogic = (
+  //   (props.mainCard === 0) ? ForecastCards.cardZero : (props.mainCard === 1) ? ForecastCards.cardOne : ForecastCards.cardTwo
+  // )
+
+  // useEffect(() => {
+  //   return (props.mainCard === 0) ? ForecastCards.cardZero : (props.mainCard === 1) ? ForecastCards.cardOne : ForecastCards.cardTwo
+  // }, [props.mainCard])
 
   return (props.geolocated === true) ? mainCard : false
 }
